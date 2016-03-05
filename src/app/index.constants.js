b60db3801cf4f3eda4ebdffd27e0eef61551d21a
Rodion <Rodion_Starodubenko@epam.com>
2016-03-05 10:42:56 +0600

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularFileUploader')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularFileUploader', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngMaterial', 'toastr']);
+
+})();
